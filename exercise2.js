7fe7a73d7b7cb880dfce5d72b3f25a6d0bfd4e94
Yanshi Agarwal <aparul742@gmail.com>
2026-01-19 22:55:31 +0530

    added files

---
+++ exercise2.js
@@ -0,0 +1,7 @@
+const strUtil = require("./stringUtils");
+
+let text = "hello world";
+
+console.log(strUtil.capitalize(text));
+console.log(strUtil.reverseString(text));
+console.log(strUtil.countVowels(text));
---
+++ stringUtils.js
@@ -0,0 +1,13 @@
+function capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1);
+}
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+
+function countVowels(str) {
+    return (str.match(/[aeiou]/gi) || []).length;
+}
+
+module.exports = { capitalize, reverseString, countVowels };
