4a73d88e3b9dc6ac369eb3b0e07e6f3b52d37556
Yanshi Agarwal <aparul742@gmail.com>
2026-02-11 15:16:30 +0530

    Updated middleware file

--- Expressjs-Backend/middleware.js
+++ Expressjs-Backend/middleware.js
@@ -7,73 +7,67 @@ app.use(express.json());
 const PORT = 8000;
 const FILE_PATH = "./students.json";
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
-
-//middleware to log request method and url   
-
-
+//LOGGER MIDDLEWARE
 app.use(async (req, res, next) => {
   try {
     const log = `${new Date().toString()} - ${req.method} - ${req.url}\n`;
-
     await fs.appendFile("log.txt", log);
-    
     next();
   } catch (err) {
     console.log("Logging error:", err);
-    next(); // server ko rukne mat do
+    next();
   }
 });
 
-app.use((req,res,next)=>{           //next:- to pass the control to the next middleware
-    console.log("I am middleware 1");
-    next()                          //Call next() to pass control to the next middleware or route handler
+//SIMPLE MIDDLEWARE
+app.use((req, res, next) => {
+  console.log("I am middleware 1");
+  next();                      //next:- to pass the control to the next middleware or route handler
 });
 
 app.use((req, res, next) => {
-    console.log("I am middleware 2");
+  console.log("I am middleware 2");
+  next();
+});
+
+//AUTH TOKEN MIDDLEWARE
+app.use((req, res, next) => {
+  const token = req.headers["authorization"];
+  if (!token) {
+    return res.status(400).send("please provide auth token");
+  }
+  if (token === "secrettoken") {
     next();
-    //return res.send("You are not allowed")
+  } else {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
 });
 
-
+//ROUTE MIDDLEWARE
 const fileAuthMiddleware = async (req, res, next) => {
-    console.log("I am checking file access");
-    return res.send("auth failed");
-}
-
-app.use((req, res, next) => {
-    const token =  req.headers["authorization"];
-    if(!token){
-        return res.status(400).send("please provide auth token" );
-    }
-    if(token === "secrettoken"){
-        next();
-    }
-    else{
-        return res.status(401).json({ message: "Unauthorized" });
-    }
-});
+  console.log("I am checking file access");
+  next();
+};
 
 const authMiddleware = (req, res, next) => {
-    console.log("I am checking auth");
-}
-
-/* ---------- HELPERS ---------- */
-const readStudentsFromFile = async () => {
-    const data = await fs.readFile(FILE_PATH, "utf-8");
-    return JSON.parse(data || "[]");
-}; 
-
-const writeStudentsToFile = async (records) => {
-  await fs.writeFile(FILE_PATH, JSON.stringify(records, null, 2));
+  console.log("I am checking auth");
+  next();
 };
 
 
-// GET all students
-app.get("/students", fileAuthMiddleware, authMiddleware,async (req, res) => {
-    const students = await readStudentsFromFile();
-    return res.status(200).json(students);
+//HELPERS
+const readStudentsFromFile = async () => {
+  const data = await fs.readFile(FILE_PATH, "utf-8");
+  return JSON.parse(data || "[]");
+};
+
+//ROUTE
+app.get("/students", fileAuthMiddleware, authMiddleware, async (req, res) => {
+  const students = await readStudentsFromFile();
+  return res.status(200).json(students);
+});
+
+//SERVER
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
